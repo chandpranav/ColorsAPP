d001d219fa4222b0daf8a1b821704eec29a91148
Pranav Chand <pranav@Pranavs-MacBook-Air-2.local>
2025-04-04 21:04:20 -0700

    Added basic UI layout with color preview and RGB SeekBars

--- app/src/main/java/com/example/colorsapp/MainActivity.kt
+++ app/src/main/java/com/example/colorsapp/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.colorsapp
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.SeekBar
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var colorView: View
+    private lateinit var seekRed: SeekBar
+    private lateinit var seekGreen: SeekBar
+    private lateinit var seekBlue: SeekBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        colorView = findViewById(R.id.colorView)
+        seekRed = findViewById(R.id.seekRed)
+        seekGreen = findViewById(R.id.seekGreen)
+        seekBlue = findViewById(R.id.seekBlue)
+
+        val listener = object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(sb: SeekBar?, progress: Int, fromUser: Boolean) {
+                updateColor()
+            }
+            override fun onStartTrackingTouch(sb: SeekBar?) {}
+            override fun onStopTrackingTouch(sb: SeekBar?) {}
         }
+
+        seekRed.setOnSeekBarChangeListener(listener)
+        seekGreen.setOnSeekBarChangeListener(listener)
+        seekBlue.setOnSeekBarChangeListener(listener)
+    }
+
+    private fun updateColor() {
+        val color = Color.rgb(seekRed.progress, seekGreen.progress, seekBlue.progress)
+        colorView.setBackgroundColor(color)
     }
 }
